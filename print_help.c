9d9521d4bb28bc6df648b0c694905619096f4814
William <william@localhost.localdomain>
2019-11-02 23:06:35 +0100

    mise a al norme

--- print_help.c
+++ print_help.c
@@ -10,7 +10,9 @@ void my_putstr(char *string);
 void print_help(void)
 {
     char *usage = "USAGE\n./calc base operators size_read\n\n";
-    char *desc = "DESCRIPTION\n- base: all the symbols of the base\n- operators: the symbols for the parentheses and the 5 operators\n- size_read: number of characters to be read\n";
+    char *desc = "DESCRIPTION\n- base: all the symbols of the base\n
+                 - operators: the symbols for the parentheses and the
+                 5 operators\n- size_read: number of characters to be read\n";
 
     my_putstr(usage);
     my_putstr(desc);
