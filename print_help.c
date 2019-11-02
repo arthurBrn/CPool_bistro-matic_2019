31d283fbe977bc74fdc4009fd14938216c871e5a
Baron <arthur.baron@epitech.eu>
2019-11-02 23:57:45 +0100

    print_help.c fixed

--- print_help.c
+++ print_help.c
@@ -10,10 +10,12 @@ void my_putstr(char *string);
 void print_help(void)
 {
     char *usage = "USAGE\n./calc base operators size_read\n\n";
-    char *desc = "DESCRIPTION\n- base: all the symbols of the base\n
-                 - operators: the symbols for the parentheses and the
-                 5 operators\n- size_read: number of characters to be read\n";
+    char *desc = "DESCRIPTION\n- base: all the symbols of the base";
+    char *snd = "- operators: the symbols for the parentheses and the";
+    char *thrd = "5 operators\n- size_read: number of characters to be read\n";
 
     my_putstr(usage);
     my_putstr(desc);
+    my_putstr(snd);
+    my_putstr(thrd);
 }
