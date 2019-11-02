e3d59951f186746eea2eee4e2560b29d7c2cd71d
Baron <arthur.baron@epitech.eu>
2019-11-02 22:45:58 +0100

    lib/parenthesis.c added

--- lib/parenthesis.c
+++ lib/parenthesis.c
@@ -8,15 +8,20 @@
 #include <stdlib.h>
 #include "my.h"
 
-int search_parenthesis_opening(char const *str, int i)
+int search_parenthesis_openning(char const *str, int i)
 {
-    while (i < 0) {
-        if (str[i] == '(') {
+    int str_len = my_strlen(str);
+
+    if (i == -1)
+        return (i);
+//        while (str[i] != '(' || str[i] != str[0]) {
+    while (str[i] != '(' || i != my_strlen(str)) {
+        if (str[i] == '(')
             return (i);
-        }
         i--;
     }
-    return (0);
+
+    return (-1);
 }
 
 int search_parenthesis_ending(char const *str)
@@ -29,32 +34,23 @@ int search_parenthesis_ending(char const *str)
         }
         i++;
     }
-    return (0);
+    return (-1);
 }
 
 char *find_expr_in_par(char const *str)
 {
-    char *a = malloc(sizeof(char) * my_strlen(str));
     int i = 0;
     int index2 = search_parenthesis_ending(str);
-    int index1 = search_parenthesis_opening(str, index2) + 1;
+    int index1 = search_parenthesis_openning(str, index2);
+    char *a = malloc(sizeof(char) * (index2 - index1) + 1);
+    int index_first_par = index1;
 
-    my_putstr("Index first : ");
-    my_putchar(index1 + '0');
-    my_putchar('\n');
-    while (index1 < index2) {
-        a[i] = str[index1];
-        index1++;
-        i++;
+    if (index2 != -1 && index1 != -1) {
+        while ((index1 + 1) <= (index2 - 1)) {
+            a[i] = str[index1 + 1];
+            index1++;
+            i++;
+        }
     }
-    my_putstr("Content of [a : ");
-    my_putstr(a);
-    my_putchar('\n');
-    my_putstr("Index first : ");
-    my_putchar(index1 + '0');
-    my_putchar('\n');
-    my_putstr("Index snd : ");
-    my_putchar(index2 + '0');
-    my_putchar('\n');
     return (a);
 }
